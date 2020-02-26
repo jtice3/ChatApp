3902b62d0779c5efd69cddab6776a5ac47000e60
jtice <jtice@users.noreply.github.com>
2020-02-25 18:43:19 -0700

    Initial Commit. Adding the project structure for a .NET Core Web Application.
    1) Add the SignalR client library.
    2) Create a SignalR hub (Hubs folder, ChatHub.cs)
    3) Configure SignalR. Edited Startup.cs
    4) Add SignalR client code. Edited 'Pages\Index.cshtml' and added 'chat.js' to wwwroot/js.

---
+++ SignalRChat/SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChat.Hubs
+{
+    /// <summary>
+    /// The ChatHub class inherits from the SignalR Hub class. 
+    /// The Hub class manages connections, groups, and messaging.
+    /// </summary>
+    public class ChatHub : Hub
+    {
+
+        /// <summary>
+        /// The SendMessage method can be called by a connected client to send a message to all clients. 
+        /// JavaScript client code that calls the method
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="message"></param>
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        
+    }
+}
---
+++ SignalRChat/SignalRChat/wwwroot/js/chat.js
@@ -0,0 +1,35 @@
+﻿/*
+ * Creates and starts a connection.
+ * Adds to the submit button a handler that sends messages to the hub.
+ * Adds to the connection object a handler that receives messages from the hub and adds them to the list.
+*/
+
+"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
